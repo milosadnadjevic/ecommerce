68ae896ce31e05fbca25f608ebb0885a3d77e517
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-11-05 23:11:13 -0500

    detail.html added

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 def categories(request):
@@ -10,3 +10,7 @@ def categories(request):
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'ecommerce/home.html', {'products': products})
+
+def product_detail (request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'products/detail.html', {'product': product})
