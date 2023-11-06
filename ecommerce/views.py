2b71b17d40e28afc790ae7c6d322444e1f413c56
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-11-05 22:39:50 -0500

    product model fixed

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Product, Category
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 
 def all_products(request):
     products = Product.objects.all()
