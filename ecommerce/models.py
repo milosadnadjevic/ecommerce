604a106a7df7834e9a078a6acd168dbe223d862f
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-11-08 07:49:15 -0500

    added link to details page

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -32,6 +34,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('ecommerce:product_detail', args=[self.slug])
 
     def __str__ (self):
         return self.title
